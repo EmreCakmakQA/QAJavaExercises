6b1183afb49ab22d91fdc1992575e7f81198be1b
EmreCakmakQA <ECakmak@qa.com>
2020-11-05 11:53:09 +0000

    EC: Added question 1 solution to repo

--- qaConditionalsExercises.java
+++ qaConditionalsExercises.java
@@ -4,7 +4,25 @@ public class qaConditionalsExercises {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+//		If the boolean is true, the method will return a sum of the two numbers; if false it will return the multiplication of the two numbers.
+//		For example:
+		
+//		Input (1, 2, true) -> 3 adds
+//		Input (3, 3, false) -> 9 multiplies
+//		Input (1, 1, true) -> 2 adds
+		
+		
+		System.out.println(input(1,1,true));
+	}
+	
+	public static int input(int number1, int number2, boolean trueOrFalse) {
+		if(trueOrFalse == true) {
+			return number1 + number2;
+		} else {
+			return number1 * number2;
+		}
 	}
 
 }
